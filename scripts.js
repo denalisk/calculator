2f4c83fa7a149f92d72e8ca44aee3fd2d97cda12
Sam Kirsch and Sandro Mijatovic <sandro@>
2017-01-09 11:45:56 -0800

    add conversion scripts from liters to other volumes

--- scripts.js
+++ scripts.js
@@ -47,3 +47,43 @@ var klToLiters = function(kiloliters) {
 var dlToLiters = function(deciliters) {
   return deiliters * .1;
 }
+
+
+
+
+
+var litersToGallons = function(liters) {
+  return liters / 3.78541;
+}
+
+var literToCups = function(liters) {
+  return liters / 0.236588;
+}
+
+var litersToOunces = function(liters) {
+  return liters / 0.0295735;
+}
+
+var litersToPints = function(liters) {
+  return liters / 0.473176;
+}
+
+var litersToQuarts = function(liters) {
+  return liters / 0.946353;
+}
+
+var litersToMl = function(liters) {
+  return liters / .001;
+}
+
+var litersToCl = function(liters) {
+  return liters / 0.01;
+}
+
+var litersToKl = function(liters) {
+  return liters / 1000;
+}
+
+var litersToDl = function(liters) {
+  return liters / .1;
+}

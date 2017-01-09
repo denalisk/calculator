2387cc9c100ab93d04b5f36e2dd11033fd756a9a
Sam Kirsch and Sandro Mijatovic <sandro@>
2017-01-09 11:00:40 -0800

    add additional funtions to the calculator scripts

--- scripts.js
+++ scripts.js
@@ -1,6 +1,28 @@
 var number1 = parseInt(prompt("Enter a number:"));
+
 var number2 = parseInt(prompt("Enter another number:"));
+
 var method = prompt("What would you like to do with the number? Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division");
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(num1, num2) {
+  return num1 - num2;
+}
+
+var multiplication = function(number1, number2) {
+  return number1 * number2;
+}
+
+var divide = function(number1, number2) {
+  return number1/number2;
+}
+
+alert(subtract(number1, number2));
+
+/*
 
 var react = method;
 
@@ -10,16 +32,4 @@ var scale = base - react;
 
 var run = function()
 
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-
-var subtract = function(num1, num2) {
-  return num1 - num2;
-
-}
-
-
-
-alert(subtract(10,3));
+*/

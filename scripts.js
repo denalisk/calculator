56218ab36ce4e6281508de937dea5d71dfa79956
Sam Kirsch and Sandro Mijatovic <sandro@>
2017-01-09 10:55:42 -0800

    add basic script and html components

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var method = prompt("What would you like to do with the number? Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division");
+
+var react = method;
+
+var base = 10;
+
+var scale = base - react;
+
+var run = function()
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+
+var subtract = function(num1, num2) {
+  return num1 - num2;
+
+}
+
+
+
+alert(subtract(10,3));

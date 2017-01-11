c998a5d22a5759f5ab47e95bf24d9419e43f9d61
Sam Kirsch and Tammy Dang <tammy@>
2017-01-11 11:14:23 -0800

    add subtraction, multiplication, division, and results form to the calculator

--- js/scripts.js
+++ js/scripts.js
@@ -7,26 +7,46 @@ $(document).ready(function() {
     var result = (add(number1, number2));
     $("#output").text(result);
   });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = (subtract(number1, number2));
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = (multiply(number1, number2));
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = (divide(number1, number2));
+    $("#output").text(result);
+  });
 });
 
+
 //Business (back-end) logic:
-var add = function(number1, number2) {
-  return number1 + number2;
+var add = function(num1, num2) {
+  return num1 + num2;
 };
 
 var subtract = function(num1, num2) {
   return num1 - num2;
 };
 
-var multiplication = function(number1, number2) {
-  return number1 * number2;
+var multiply = function(num1, num2) {
+  return num1 * num2;
 };
 
-var divide = function(number1, number2) {
-  return number1/number2;
+var divide = function(num1, num2) {
+  return num1/num2;
 };
-
-alert(subtract(number1, number2));
 
 /*
 
